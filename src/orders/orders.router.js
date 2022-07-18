a2138641a22d259865271d41c8b10640286148c9
Lev_Gordon <lev.gordonsa@gmail.com>
2022-07-18 13:24:34 -0700

    final commit

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,25 +1,22 @@
 const router = require("express").Router();
+const controller = require("../orders/orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /orders routes needed to make the tests pass
 
 router
-.route("/")
-// to make a GET request that uses list()
-.get(controller.list)
-// make POST request to create new dish using create()
-.post(controller.create)
-// for request types that aren't available to use on dish
-.all(methodNotAllowed)
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed)
+;
 
 router
-.route("/:orderId")
-// to make a GET request that uses read()
-.get(controller.read) 
-// makes PUT request that uses update() to update a dish
-.put(controller.update)
-// for request types that aren't available to use on dish
-.delete(controller.delete) //  
-.all(methodNotAllowed)
-
+  .route("/:orderId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed)
+;
 
 module.exports = router;
